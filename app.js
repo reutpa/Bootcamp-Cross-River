199e63d7068b048203044be05b458d226919a22a
reutpa <reut08558@gmail.com>
2022-07-06 12:19:05 +0300

    this is the first exercise in promise

--- app.js
+++ app.js
@@ -4,22 +4,3 @@ let promiseRandom = () => {
         setTimeout(() => reject('I failed!'), 5000);
     }).then(alert);
 }
-
-let makeAllCaps = (arr) => {
-    return new Promise((resolve, reject) => {
-        resolve(arr.map(element => {
-            return element.toUpperCase();
-        }), arr);
-        reject('this array is not contains only strings!')
-    });
-}
-
-let sortWords = (arr) => {
-    return new Promise((resolve, reject) => {
-        resolve(makeAllCaps(arr).then(result => { result.sort(); return result; }),
-            error => reject(error));
-    });
-}
-
-let arr = ["ccc", "bbb", "aaa", "abc", "def", "ddd", "eee", "eff"];
-sortWords(arr).then(data => console.log(data));
